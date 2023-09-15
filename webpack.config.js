183d8f5eadf2545a7df4128740e69983e6058249
nanakmaulik <nanakmaulik@gmail.com>
2023-09-15 22:58:38 +0530

    adding files

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: './your-entry-file.js',
+    output: {
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
+  };
+  
